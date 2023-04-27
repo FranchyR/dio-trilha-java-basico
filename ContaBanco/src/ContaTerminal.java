20b1c642d12a82c64336f7460f7bcb39c30a8ecf
= <=>
2023-04-27 19:57:18 -0300

    classe completa

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.printf("Por favor, digite o número da Conta!\n");
+        numero = sc.nextInt();
+
+        System.out.printf("Por favor, digite o número da Agência!\n");
+        agencia = sc.next();
+        
+        sc.nextLine();
+
+        System.out.printf("Por favor, digite o Nome do Cliente!\n");
+        nomeCliente = sc.nextLine();
+        
+        System.out.printf("Por favor, digite o Saldo da Conta!\n");
+        saldo = sc.nextDouble();        
+
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.",
+                                  nomeCliente, agencia, numero, saldo);
+                            
+        sc.close();
     }
 }
